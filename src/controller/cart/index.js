0639f54cb6d46658169aed449786347c7a43960b
sixtusiwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-06-02 11:21:03 +0100

    update

--- src/controller/cart/index.js
+++ src/controller/cart/index.js
@@ -3,23 +3,34 @@ const mongoose = require("mongoose");
 const __Cart = require("../../models/cart/cart");
 
 module.exports = class CartController {
-  async getUserCart(req) {
-    if (!req.user) {
-      return "login to continue";
+    async getUserCart(req) {
+        if (!req.user) {
+            return "login to continue";
+        }
+        return __Cart.find({userId: req.user._id});
+
     }
-    let userCart = await __Cart.find({userId:req.user._id})
-    return userCart;
-  }
+
     async AddToCart(req) {
-    if (!req.user) {
-      return "login to continue";
+        if (!req.user) {
+            return "login to continue";
+        }
+        try {
+            if (req.body.offline) {
+                for (const cartItem of req.body.offline) {
+                    await __Cart.create({cartItem, userId: req.user._id})
+                }
+                const response = await __Cart.find({userId: req.user._id});
+                return response
+            } else {
+                await __Cart.create({...req.body, userId: req.user._id})
+                const response = await __Cart.find({userId: req.user._id});
+                return response
+
+            }
+        } catch (err) {
+            console.log(err)
+            return ("internal server error")
+        }
     }
-  try{
-     await __Cart.create({...req.body,userId:req.user._id})
-    return "added to cart";
-  }catch(err){
-      console.log(err)
-      return ("internal server error")
-  }
-}
 };
