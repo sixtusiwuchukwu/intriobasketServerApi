b292d887c6b0099dbfd2b6366dc2a85d098ace29
sixtusiwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-06-12 08:38:15 +0100

    update

--- src/route/Product/index.js
+++ src/route/Product/index.js
@@ -36,30 +36,6 @@ router.post("/create", isAuth, async (req, res) => {
   let result = await new ProductController().createProduct(req);
   return res.send(result);
 });
-
-router.put("/:id", isAuth,async (req, res) => {
-  const { id } = req.params;
-  if (!id | (id === "")) {
-    return res.send("product id must be provided");
-  }
-  let result = await new ProductController().Updateproduct(req,id);
-  return res.send(result);
-});
-
-router.delete("/:id", isAuth,async (req, res) => {
- 
-  if (!req.user) {
-    return res.send("please log in to continue");
-  }
-  const { id } = req.params;
-  if (!id | (id === "")) {
-    return res.send("product id must be provided");
-  }
-  let result = await new ProductController().deleteProduct(req,id);
-
-  return res.send(result);
-});
-
 router.post("/createcategory", isAuth, async (req, res) => {
   if (!req.user) {
     return res.send("please log in to continue");
@@ -73,12 +49,25 @@ router.post("/createcategory", isAuth, async (req, res) => {
 
   return res.send(result);
 });
-
 router.get("/categories",async(req,res)=>{
   let result = await new ProductController().getCategories()
 res.send(result)
 })
+router.get("/recent",async(req,res)=>{
+  let result = await new ProductController().getRecentSold(req)
+  res.send(result)
+})
+router.put("/updatestatus",isAuth,async(req,res)=>  {
+  let result = await new ProductController().updateProductStatus(req)
+  res.send(result)
+})
+router.get("/category/:categoryname", async (req, res) => {
+  const { categoryname } = req.params;
 
+  let result = await new ProductController().getCategory(categoryname);
+
+  return res.send(result);
+});
 router.get("/search/:product", async (req, res) => {
   const { product } = req.params;
   if (!product | (product === "")) {
@@ -88,37 +77,42 @@ router.get("/search/:product", async (req, res) => {
 
   return res.send(result);
 });
-router.get("/category/:categoryname", async (req, res) => {
-  const { categoryname } = req.params;
-
-  let result = await new ProductController().getCategory(categoryname);
-
-  return res.send(result);
-});
-
-router.get("/recent",async(req,res)=>{
-  let result = await new ProductController().getRecentSold(req)
-  res.send(result)
-})
-router.put("/updatestatus",isAuth,async(req,res)=>{
-  let result = await new ProductController().updateProductStatus(req)
-  res.send(result)
-})
 router.get("/admin/:query",isAuth,async(req,res)=>{
   let result = await new ProductController().Adminproducts(req)
   res.send(result)
 })
+router.put("/:id", isAuth,async (req, res) => {
+  const { id } = req.params;
+  if (!id | (id === "")) {
+    return res.send("product id must be provided");
+  }
+  let result = await new ProductController().Updateproduct(req,id);
+  return res.send(result);
+});
 
+router.delete("/:id", isAuth,async (req, res) => {
+
+  if (!req.user) {
+    return res.send("please log in to continue");
+  }
+  const { id } = req.params;
+  if (!id | (id === "")) {
+    return res.send("product id must be provided");
+  }
+  let result = await new ProductController().deleteProduct(req,id);
+
+  return res.send(result);
+});
 //this endpoint was a promblem
-// router.get("/:id", async (req, res) => {
-//   const { id } = req.params;
-//   if (!id | (id === "")) {
-//     return res.send("product id must be provided");
-//   }
-//   let result = await new ProductController().Getproduct(req,id);
-//
-//   return res.send(result);
-// });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id | (id === "")) {
+    return res.send("product id must be provided");
+  }
+  let result = await new ProductController().Getproduct(req,id);
+
+  return res.send(result);
+});
 
 
 
