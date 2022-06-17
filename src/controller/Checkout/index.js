b1aa3cf70ef810ef5a1640c824e7d3624229a9c3
sixtusiwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-06-17 12:12:01 +0100

    update

--- src/controller/Checkout/index.js
+++ src/controller/Checkout/index.js
@@ -40,7 +40,7 @@ module.exports = class ProductController {
     if (!req.user) {
       return "login to continue";
     }
-    return await __CheckOut.find({ _id:req.params.Id}).populate("products.product")
+    return await __CheckOut.findOne({ _id:req.params.Id}).populate("products.product")
   }
   async updateCheckOutStatus(req){
     if (!req.user) {
